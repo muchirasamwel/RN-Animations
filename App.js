481d6776cd1fcf6a8e7316d0426d874dd2df8ef7
Samwel Muchira <muchirasamwel@gmail.com>
2021-09-11 19:54:28 +0300

    init comm

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar'
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useRef } from 'react'
+import {
+  Animated,
+  Easing,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+import DeatailsLoader from './AnimationScreens/DetailsLoader'
+import HorizonScroll from './AnimationScreens/HorizonScroll'
+import ScrollMastery from './AnimationScreens/ScrollMastery'
 
 export default function App () {
-  const [translation, setTranslation] = useState(0)
+  const translation = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
+  // const animeColor = useRef(new Animated.Value('red')).current
+  useEffect(() => {
+    // for (let i = 0; i <= 100; i++) {
+    //   setTimeout(() => {
+    //     translation.setValue(i)
+    //   }, 25 * i)
+    // }
+  })
+  const touchHandler = () => {
+    // Animated.timing(translation.x, {
+    //   toValue: 100,
+    //   duration: 500,
+    //   easing: Easing.bounce,
+    //   useNativeDriver: true
+    // })
+    Animated.sequence([
+      // Animated.stagger([]),
+      Animated.timing(translation.x, {
+        toValue: 100,
+        duration: 1000,
+        // easing: Easing.bounce,
+        useNativeDriver: true
+      }),
+      Animated.timing(translation.y, {
+        toValue: 100,
+        duration: 500,
+        easing: Easing.bounce,
+        useNativeDriver: true
+      })
+    ]).start()
+  }
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'red',
-          transform: [{ translateX: translation }]
-        }}
-      ></View>
-    </View>
+    // <ScrollMastery />
+    // <DeatailsLoader />
+    <HorizonScroll />
+    // <ScrollView>
+    //   <View style={styles.container}>
+    //     {/* <Animated.View
+    //     onTouchEnd={touchHandler}
+    //     style={{
+    //       width: 100,
+    //       height: 100,
+    //       backgroundColor: 'red',
+    //       transform: [
+    //         { translateX: translation.x },
+    //         { translateY: translation.y }
+    //       ]
+    //     }}
+    //   ></Animated.View> */}
+    //     <Animated.View
+    //       onTouchEnd={touchHandler}
+    //       style={{
+    //         width: 100,
+    //         height: 100,
+    //         backgroundColor: 'blue',
+    //         opacity: translation.x.interpolate({
+    //           inputRange: [0, 50, 100],
+    //           outputRange: [1, 0, 1]
+    //         }),
+    //         transform: [
+    //           { translateX: translation.x },
+    //           { translateY: translation.y }
+    //         ]
+    //       }}
+    //     ></Animated.View>
+    //   </View>
+    // </ScrollView>
   )
 }
 
